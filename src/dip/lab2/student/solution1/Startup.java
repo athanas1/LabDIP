f0530dd846c5401e99646f8a6c71f176ce13bcbe
Alex <Alex@Alex-PC.attlocal.net>
2016-02-09 14:04:50 -0600

    Fixed lab2

--- src/dip/lab2/student/solution1/Startup.java
+++ src/dip/lab2/student/solution1/Startup.java
@@ -1,37 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package dip.lab2.student.solution1;
 
-// An useful import if you need it.
-import dip.lab2.*;
-import java.text.NumberFormat;
-// Another useful import if you need it.
-import javax.swing.JOptionPane;
-
 /**
- * Just a test class for input and output.
  *
- * 1. Create instances of low-level classes using Liskov Substitution principle.
- * 2. Create an instance of your high-level class.
- * 3. Pass one of your calculator instances to the high-level class and
- *    check the results.
- * 4. Now switch to a different calculator instance and pass that to the
- *    high-level class. Did it work? Are the low-level instances
- *    interchangeable? The DIP requires this.
- * 
- * @author your name goes here
+ * @author athanas1
  */
 public class Startup {
-    public static enum ServiceQuality {
-           GOOD, FAIR, POOR
-    };
- 
-    public static void main(String[] args) {
-        TipCalculator tip1 = new BaggageServiceTipCalculator(BaggageServiceTipCalculator.ServiceQuality.GOOD, 20 );
-        TipCalculator tip2 = new FoodServiceTipCalculator(FoodServiceTipCalculator.ServiceQuality.POOR, 10);
+     
+    
+   public static void main(String[] args) {
+        TipCalculator tip1 = new FoodTipCalculator(ServiceQuality.GOOD, 45);
         
-        TipCalculator tipcalc[] = {tip1, tip2};
+        Tip tp = new Tip();
         
-        
-         Tip lab1 = new Tip();
+        System.out.println(tp.getTipForEmployee(tip1));
     }
-
 }
+
---
+++ src/dip/lab2/student/solution1/Tip.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dip.lab2.student.solution1;
+
+import dip.lab2.*;
+
+/**
+ *
+ * @author Alex
+ */
+public class Tip {
+    public double getTipForEmployee (TipCalculator TC){
+        return TC.getTip();
+    }
+}
